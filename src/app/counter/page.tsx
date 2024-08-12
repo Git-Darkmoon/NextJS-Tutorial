969558235a6b2badf7d7772f9ce3555cbe6317bc
Nicolas Mayorga <namayorgav@gmail.com>
2024-08-12 15:14:34 -0500

    Lesson 550: Fetching Data.

---
+++ src/app/counter/page.tsx
@@ -0,0 +1,11 @@
+import Counter from "@/components/Counter"
+
+function CounterPage() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-10">Counter Page</h1>
+      <Counter />
+    </div>
+  )
+}
+export default CounterPage
